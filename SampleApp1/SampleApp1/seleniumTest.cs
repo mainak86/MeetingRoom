a88a8ce66631ad89d55a29fbf09120c563851289
mainak86 <mainakatreading@gmail.com>
2019-02-19 17:15:30 +0530

    adding selenium test

---
+++ SampleApp1/SampleApp1/seleniumTest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleApp1
+{
+    public class seleniumTest
+    {
+        IWebDriver driver;
+        static void Main(string[] ss) {
+
+        }
+       
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\3rdparty\\chrome");
+        }
+       
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+       
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SampleApp1/SeleniumTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        public IWebDriver driver;
+
+        static void Main(string[] args)
+        {
+            //Program prg = new Program();
+            //prg.startBrowser();
+            //prg.test();
+        }
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver(@"C:\temp");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://localhost:50093"; /*"https://jenkinstestseleniumapp.azurewebsites.net";*/
+            //driver.FindElement(By.Name("q")).SendKeys("Azure Portal");
+            Assert.True(driver.FindElement(By.Name("btnK")).Text == "Learn more »");
+            //closeBrowser();
+           // Assert.True(1==1);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
