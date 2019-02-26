207ab5ba000b1193cfa87b669e88dce754e458f0
mainak86 <mainakatreading@gmail.com>
2019-02-26 14:06:34 +0530

    Update HomeController.cs

--- DemoApp1/DemoApp1/Controllers/HomeController.cs
+++ DemoApp1/DemoApp1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DemoApp1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page changed.";
 
             return View();
         }
